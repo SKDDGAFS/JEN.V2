82af97376c9d535dac20ba0478aff2b45da39573
SKDDGAFS <abcdxyqo@proton.me>
2026-07-31 19:37:03 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -62,7 +62,7 @@ export const sceneConfig = {
   musicContinueText: 'Continue to music',
   closingContinueText: 'Close the chapter',
   closingTitle: 'GOOSBYEEE',
-  closingText: 'JENNY I MISS YOU SOOO MUCH. ITS RN 6:56pm im coding this through my phone bc my laptops died. Jenny i dindt do it on purpose to let y on delivered.  ut y hurt me when u say i am cheeting',
+  closingText: 'JENNY I MISS YOU SOOO MUCH. ITS RN 6:56pm im coding this through my phone bc my laptops died. Jenny i dindt do it on purpose to let y on delivered, but y hurt me when u say i am cheeting. I LIKE YOU SO MUCH ❤️',
   closingResetText: 'Restart',
   artworks: [
     {
